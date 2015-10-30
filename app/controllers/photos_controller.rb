ac8ce3485f9cca67bede537967f289e207285c2e
emlee9017 <elee18@chicagobooth.edu>
2015-10-29 21:15:50 -0500

    HW Part 1

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,41 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+  	@p=Photo.find(params[:id])
+  end
 
+  def new_form
+  end
+
+  def create_row
+  	@p = Photo.new
+  	@p.caption = params[:caption]
+    @p.source = params[:image_url]
+    @p.save
+
+  	redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @p = Photo.find(params[:id])
+  end
+
+  def update_row
+    @p = Photo.find(params[:id])
+    @p.caption = params[:caption]
+    @p.source = params[:image_url]
+    @p.save
+
+    render("show.html.erb")
+  end
+
+  def destroy
+    @p = Photo.find(params[:id])
+
+    @p.destroy
+  end
 end
+
+
+  
